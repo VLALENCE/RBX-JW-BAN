f8ea58a558f458410c1d4add0f835ec764d5c041
OfficialRBXAA <official.alphaauthority@gmail.com>
2022-08-07 21:04:27 -0500

    V1 version

--- loaderHierarchyHead/MainModule.lua
+++ loaderHierarchyHead/MainModule.lua
@@ -1,76 +1,96 @@
 --[[
 
 Created by ScriptIntelligence
-Started January 2nd, 2021 (MainModule Version)
-V2 Started August 7th, 2022 (Firebase Version)
+Started January 2nd, 2021
 
 Property of Alpha Authority
 
 ]]
 
---[[
-
--- Redacted -- Internal processing instructions for Robase authorization hidden. 
-
-]]
-
---
-
 script.Name = 'Blacklist / Whitelist Main Module'
-
--- 
 
 warn(script.Name .. ' ~ Starting Blacklist / Whitelist Main Module')
 
---
-
 local PlayerService = game:GetService('Players')
-local GroupService = game:GetService('GroupService')
--- Redacted -- Internal processing service for Robase authorization hidden.  
 
---
+local Blacklist = {}
 
--- Redacted -- Internal processing for Robase authorization hidden.  
-local RobaseServiceModule = require(script.RobaseService) 
-local RobaseService = RobaseServiceModule.new(--[[Redacted]]) -- Internal processing for Robase authorization hidden. 
+Blacklist.Players = {
+	18745095,
+}
 
-local RobaseUserBlacklist = RobaseService:GetRobase('blacklist')
-local RobaseUserWhitelist = RobaseService:GetRobase('whitelist')
+-- 18745095 - natsuflux
 
---
+Blacklist.Groups = {
+	3064711,
+	9603500,
+	7881372,
+	2969248,
+	4733250,
+	4665428,
+	5234523,
+	4105128,
+	5889407,
+	2732679,
+	5133149,
+	4963120,
+	58267,
+	3261533,
+	5340099
+}
+
+-- 3064711 - A-NEXO
+-- 9603500 - Bloxxer Liberation Army
+-- 7881372 - Cursed Awoken
+-- 2969248 - Combat Assault Team 
+-- 4733250 - Darkness Reborn :
+-- 4665428 - Dark Valiant
+-- 5234523 - Demon Sovereignty
+-- 4105128 - 𝐄jército Mexicano
+-- 5889407 - Fallen Veil
+-- 2732679 - Federation of Arcadia
+-- 5133149 - 2Hard | Alt Bloodline
+-- 4963120 - Imp3ri4l Int3llig3nc3
+-- 58267 - ROBLOX Bloxxers United
+-- 3261533 - st0rmtr00p3rz
+-- 5340099 - The Genesis Imperial
+
+local Whitelist = {}
+
+Whitelist.Players = {
+	32098161,
+	17147722,
+}
+
+-- 32098161 - Eze44
+
+Whitelist.Groups = {
+	6884402
+}
+
+-- 6884402 - Crimsοn Eyes
 
 local function kickPlayer(Player)
 	Player:Kick('Blacklisted!')
-	warn(script.Name .. ' ~ Kicked ' .. Player.Name .. ", blacklisted individual.")
+	error(script.Name .. ' ~ Kicked ' .. Player.Name .. ", blacklisted individual.")
 end
 
 --
 
 local function checkBlacklist(Player)
 	print(script.Name .. ' ~ Checking Blacklist for ' .. Player.Name)
-	--if table.find(Blacklist.Players, Player.UserId) then return true end
-	local Success, RobaseUserBlacklistUsers = RobaseUserBlacklist:GetAsync("users")
-	for _,User in pairs(RobaseUserBlacklistUsers) do
-		if User['userId'] == Player.UserId then return true end
+	if table.find(Blacklist.Players, Player.UserId) then return true end
+	for _,Group in ipairs(Blacklist.Groups) do
+		if Player:IsInGroup(Group) then return true end
 	end
-	--for _,Group in ipairs(Blacklist.Groups) do
-	local Success, RobaseUserBlacklistGroups = RobaseUserBlacklist:GetAsync("groups")
-	for _,Group in pairs(RobaseUserBlacklistGroups) do
-		if Player:IsInGroup(Group['groupId']) then return true end
-	end
+	return false
 end
 
 local function checkWhitelist(Player)
 	print(script.Name .. ' ~ Checking Whitelist for ' .. Player.Name)
-	--if table.find(Whitelist.Players, Player.UserId) then print(script.Name .. ' ~ ' .. Player.Name .. ' is Whitelisted.') return true end
-	local Success, RobaseUserWhitelistUsers = RobaseUserWhitelist:GetAsync("users")
-	for _,User in pairs(RobaseUserWhitelistUsers) do
-		if User['userId'] == Player.UserId then print(script.Name .. ' ~ ' .. Player.Name .. ' is Whitelisted.') return true end
-	end
-	--for _,Group in ipairs(Whitelist.Groups) do
-	local Success, RobaseUserWhitelistGroups = RobaseUserWhitelist:GetAsync("groups")
-	for _,Group in pairs(RobaseUserWhitelistGroups) do
-		if Player:IsInGroup(Group['groupId']) then print(script.Name .. ' ~ ' .. Player.Name .. ' is Whitelisted.') return true end
+	if table.find(Whitelist.Players, Player.UserId) then print(script.Name .. ' ~ ' .. Player.Name .. ' is Whitelisted.') return true end
+	for _,Group in ipairs(Whitelist.Groups) do
+		if Player:IsInGroup(Group) then print(script.Name .. ' ~ ' .. Player.Name .. ' is Whitelisted.') return true end
 	end
 end
 
@@ -86,11 +106,11 @@ end
 
 local function intiliaze()
 	PlayerService.PlayerAdded:Connect(function(Player)
-		checkPlayer(Player)    
+		checkPlayer(Player)	
 	end)
 	for index,player in ipairs(PlayerService:GetPlayers()) do
 		checkPlayer(player)
-	end    
+	end	
 end
 
 --
