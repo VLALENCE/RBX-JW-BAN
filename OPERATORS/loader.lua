606c2015d89f78978b7dccd90ce236adcac3374e
Cynthia Vlalence <50034413+VLALENCE@users.noreply.github.com>
2025-07-25 17:19:38 -0500

    JW Version

--- OPERATORS/loader.lua
+++ OPERATORS/loader.lua
@@ -5,17 +5,21 @@ Started January 2nd, 2021 (MainModule Version)
 V2 Started August 7th, 2022 (Firebase Version)
 V3 Started May 27, 2024 (Secrets & Firebase Version)
 V4.0 Started July 3, 2025 (Github Version)
+V4.0.JW Started July 25, 2025 (JW Github Version)
 
-Dedicated by ScriptIntelligence to Trenati
+Dedicated by ScriptIntelligence to the public
 
 ]]
 
+
+--
+
 local function blacklist()
-	warn(script.Name .. ' ~ Requiring Blacklist / Whitelist Main Module')
+	warn(script.Name .. ' ~ Requiring JW Blacklist Main Module')
 	if script:FindFirstChild("MainModule") then
 		require(script.MainModule)
 	else
-		require(6288699842)
+		require(77856084120340)
 	end
 end
 
---
+++ README.md
@@ -0,0 +1,1 @@
+Automatic account banning of JackRuin accounts. 
